8af4efcd52ef146813a6652a4fe63c142f2d7f5a
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-19 13:30:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-/* Code that print Programming is like building . */
-#include(stdio.h)
-int main(void) 
+#include <stdio.h>
+/* This code will print what is inside the puts functions */
+int main(void) {
        	puts("Programming is like building a multilingual puzzle"\n);
 	return(0);
 }
