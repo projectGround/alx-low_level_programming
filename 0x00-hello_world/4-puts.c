dec15071304d18d10a6c3da414b2eb5e6f94518d
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-19 13:38:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* This code will print what is inside the puts functions */
+/**
+ * main - A program that print a line with put function
+ * Return:  The moment this programe return with 0 it end the code 
+ */
 int main(void) {
        	puts("Programming is like building a multilingual puzzle"\n);
 	return(0);
