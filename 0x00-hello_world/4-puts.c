22f44225ecce2b2de48b52a11ac6e0a417dddae6
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-19 15:20:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - This code print a single line with puts functions 
  * Return: 0 (Success)
 */
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts(" \"Programming is like building a multilingual puzzle" );
 return(0);
 }
