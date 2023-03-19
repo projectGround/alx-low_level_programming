4bd38253c4fa69d41f8a9a581ca6285667384bc4
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-19 14:37:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /* This code print a single line with puts functions */
-int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void) 
+{
+	puts (" \"Programming is like building a multilingual puzzle");
 	return(0);
 }
