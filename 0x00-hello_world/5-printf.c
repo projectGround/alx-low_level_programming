aa789714916a7fa08bc2077c86f558f2d423ee93
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-19 16:57:20 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
-* Using a function print function printing a single line 
-* Return: 0
-*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
-echo %return: =%;
 }
