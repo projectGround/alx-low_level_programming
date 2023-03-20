1ba1599a7f8480d83a21a0c7dd4fb4ba6f24d5f0
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-20 14:38:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main Writing a C file that print a line of code 
+ * Return: success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
