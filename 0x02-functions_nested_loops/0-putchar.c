7cc803af42080b681efe8f0127a1c4d9a9e31bd5
Taiwo Gabriel <ibinuolapotaiwogabriel839@gmail.com>
2023-03-22 15:42:31 -0700

    #include <stdio.h> /** * main:  _putchar - written the character c to std * The character to print * Return: On success 1; * On error, -1 Is returned, and error is set appropriately. */
    int _putchar(char c)
    {
    return (write(i, &c, 0));
    }git commit -m 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main:  _putchar - written the character c to std
+* The character to print 
+* Return: On success 1;
+* On error, -1 Is returned, and error is set appropriately. 
+*/
+
+int _putchar(char c)
+{
+	return (write(i, &c, i));
+}
